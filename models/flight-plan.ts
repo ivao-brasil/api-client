ff63214b67766ab523e6965fac6f7aad8608345f
Luciano Santos <luciano.ssants@gmail.com>
2021-01-30 05:58:34 -0300

    fix client according swagger doc

--- models/flight-plan.ts
+++ models/flight-plan.ts
@@ -11,13 +11,94 @@
  * https://github.com/swagger-api/swagger-codegen.git
  * Do not edit the class manually.
  */
-
-import { FlightPlanInner } from ".";
-
 /**
  * 
  * @export
  * @interface FlightPlan
  */
-export interface FlightPlan extends Array<FlightPlanInner> {
+export interface FlightPlan {
+    /**
+     * 
+     * @type {string}
+     * @memberof FlightPlan
+     */
+    validSince?: any;
+    /**
+     * 
+     * @type {string}
+     * @memberof FlightPlan
+     */
+    validUntil?: any;
+    /**
+     * 
+     * @type {string}
+     * @memberof FlightPlan
+     */
+    days?: any;
+    /**
+     * 
+     * @type {string}
+     * @memberof FlightPlan
+     */
+    callsign?: any;
+    /**
+     * 
+     * @type {string}
+     * @memberof FlightPlan
+     */
+    acft?: any;
+    /**
+     * 
+     * @type {string}
+     * @memberof FlightPlan
+     */
+    dep?: any;
+    /**
+     * 
+     * @type {string}
+     * @memberof FlightPlan
+     */
+    eobt?: any;
+    /**
+     * 
+     * @type {string}
+     * @memberof FlightPlan
+     */
+    spd?: any;
+    /**
+     * 
+     * @type {number}
+     * @memberof FlightPlan
+     */
+    fl?: any;
+    /**
+     * 
+     * @type {string}
+     * @memberof FlightPlan
+     */
+    route?: any;
+    /**
+     * 
+     * @type {string}
+     * @memberof FlightPlan
+     */
+    dest?: any;
+    /**
+     * 
+     * @type {string}
+     * @memberof FlightPlan
+     */
+    eet?: any;
+    /**
+     * 
+     * @type {string}
+     * @memberof FlightPlan
+     */
+    rmk?: any;
+    /**
+     * 
+     * @type {string}
+     * @memberof FlightPlan
+     */
+    logo?: any;
 }
